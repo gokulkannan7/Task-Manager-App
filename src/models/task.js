9e4195511a8c251795c932fba3b61044510e292c
gokulkannan7 <gokulrao31@gmail.com>
2021-03-31 19:29:19 +0530

    files added

---
+++ src/models/task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const TaskSchema = mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User' // The exact name of the Model which u want it to reference to
+
+    }
+},{
+    timestamps:true
+})
+
+
+const Task = mongoose.model('Task',TaskSchema)
+
+module.exports = Task
